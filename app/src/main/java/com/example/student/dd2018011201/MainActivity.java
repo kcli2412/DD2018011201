3b5588b1b90dc4a7c3ae9add74c1af3952948688
kcli2412 <kcli2412@gmail.com>
2018-01-12 10:04:04 +0800

    Queue & Request

--- app/src/main/java/com/example/student/dd2018011201/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018011201/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.student.dd2018011201;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest("https://www.mobile01.com/rss/news.xml",
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("NET", response);
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+        queue.add(request);
+        queue.start();
     }
 }
