736745e7c93c1b78bd4645064f0e20989497a048
kcli2412 <kcli2412@gmail.com>
2018-01-12 10:13:26 +0800

    JSON

--- app/src/main/java/com/example/student/dd2018011201/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018011201/MainActivity.java
@@ -11,6 +11,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,11 +26,19 @@ public class MainActivity extends AppCompatActivity {
     public void click1(View v)
     {
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-        StringRequest request = new StringRequest("https://www.mobile01.com/rss/news.xml",
+        StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
                 new Response.Listener<String>(){
                     @Override
                     public void onResponse(String response) {
                         Log.d("NET", response);
+                        try {
+                            JSONArray array = new JSONArray(response);
+                            JSONObject obj = array.getJSONObject(0);
+                            String str = obj.getString("district");
+                            Log.d("NET", str);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener(){
